ffce69cf9dd5bdfbfe5577ed3b6e0e51cd73ff9e
tmmarques <trmarques@fc.ul.pt>
2019-12-05 05:16:36 +0000

    documentation added

--- R/praiseme.R
+++ R/praiseme.R
@@ -1,3 +1,15 @@
+#' A function that praises the user
+#'
+#' \code{praiseme} returns praise incorporating a user-specified string.
+#'
+#' @param your_name A string that is your name.
+#'
+#' @return A string with the praise
+#' @export
+#'
+#' @examples
+#'
+#' praiseme("John")
 praiseme <- function (your_name){
 
     paste("Hey", your_name, "you rock!!!")
